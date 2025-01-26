be95551dfbe32cda7bfd3810bd07d12f8caabc78
comma <useyourcommas@gmail.com>
2025-01-26 14:44:08 +0000

    blinking and waving script

--- scripts/main.js
+++ scripts/main.js
@@ -1,15 +1,38 @@
 const polaroid = document.querySelector("img");
+let flag = 0;
 
 polaroid.addEventListener("click", () => {
-    const polaroidSrc = polaroid.getAttribute("src");
-    if(polaroidSrc === "images/commaSpringClosed.png") {
-        polaroid.setAttribute("src", "images/commaSpringOpen.png");
-    } else {
+    if (flag == 1) return;
+    flag = 1;
+
+    polaroid.setAttribute("src", "images/commaSpringClosed.png");
+
+    setTimeout(() => {
+        polaroid.setAttribute("src", "images/commaSpringBent.png");
+    }, 100);
+
+    setTimeout(() => {
+        polaroid.setAttribute("src", "images/commaSpringWave.png");
+    }, 150);
+
+    setTimeout(() => {
+        polaroid.setAttribute("src", "images/commaSpringBent.png");
+    }, 900);
+
+    setTimeout(() => {
         polaroid.setAttribute("src", "images/commaSpringClosed.png");
-    }
+    }, 950)
+
+    setTimeout(() => {
+        polaroid.setAttribute("src", "images/commaSpringOpen.png");
+    }, 1000);
+
+    flag = 0;
 });
 
 function blink() {
+    flag = 1;
+
     polaroid.setAttribute("src", "images/commaSpringBlink.png");
 
     setTimeout(() => {
@@ -23,6 +46,8 @@ function blink() {
     setTimeout(() => {
         polaroid.setAttribute("src", "images/commaSpringOpen.png")
     }, 550);
+
+    flag = 0;
 }
 
 setInterval(blink, 5000);
