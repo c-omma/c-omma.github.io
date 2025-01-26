2af77bd4e44b84fe9fe82103801cfc5b6f051f5b
comma <useyourcommas@gmail.com>
2025-01-26 16:29:56 +0000

    fixes to banner script

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,9 @@
+/* banner for h2 */
 const banner = document.querySelector("h2");
 let bannerLetters = Array.from(banner.textContent);
-console.log(bannerLetters);
+// console.log(bannerLetters);
+
+bannerLetters.forEach((letter, index) => {
+    if(index === 0) banner.textContent = letter;
+    else banner.textContent += " " + letter;
+});
